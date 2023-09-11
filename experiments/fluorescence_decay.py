e6b8f861267aa51a98ab624b4731ea27e2ef0f59
Vutha lab @ UofT <electric.atoms@gmail.com>
2023-09-11 19:47:42 -0400

    Update fluorescence_decay.py

--- experiments/fluorescence_decay.py
+++ experiments/fluorescence_decay.py
@@ -1,8 +1,5 @@
 import time
 import os
-import contextlib
-import io
-import sys
 
 import numpy as np
 
@@ -12,7 +9,7 @@ os.environ["DATAFOLDER"] = "/home/onix/Documents/data"
 from onix.data_tools import save_experiment_data
 from onix.units import ureg
 
-from onix.headers.digitizer import Digitizer
+from onix.headers.digitizer import DigitizerVisa
 from onix.headers.wavemeter.wavemeter import WM
 from onix.headers.awg.M4i6622 import M4i6622
 
@@ -26,11 +23,6 @@ def wavemeter_frequency():
     return freq
 
 wavemeter = WM()
-try:
-    dg = Digitizer("192.168.0.125")
-    dg.list_errors()
-except Exception:
-    dg = Digitizer("192.168.0.125")
 
 ## experiment parameters
 excitation_aom_channel = 2
@@ -52,11 +44,11 @@ repeats = 1000
 sampling_rate = 1e6
 
 ## setup sequence
-sequence = Sequence(awg_channel_num=4, ttl_channels=[0])
+sequence = Sequence()
 
 segment_test = Segment("test")
 offset = 100 * ureg.us
-ttl_gate = TTLPulses([[0, offset.to("s").magnitude]])
+ttl_gate = TTLPulses([[0, offset]])
 segment_test.add_ttl_function(pmt_gate_ttl_channel, ttl_gate)
 pulse_test = AWGSinePulse(
     frequency=test_aom_frequency,
@@ -65,7 +57,7 @@ pulse_test = AWGSinePulse(
     end_time=offset*2+excitation_delay+test_time,
 )
 segment_test.add_awg_function(excitation_aom_channel, pulse_test)
-segment_test._duration = segment_test.duration + offset.to("s").magnitude
+segment_test._duration = segment_test.duration + offset
 sequence.add_segment("test", segment_test)
 
 segment_excitation = Segment("excite")
@@ -76,65 +68,39 @@ pulse_excitation = AWGSinePulse(
     end_time=excitation_delay + excitation_time,
 )
 segment_excitation.add_awg_function(excitation_aom_channel, pulse_excitation)
-ttl_gate = TTLPulses([[0, (2 * excitation_delay + excitation_time).to("s").magnitude]])
+ttl_gate = TTLPulses([[0, 2 * excitation_delay + excitation_time]])
 segment_excitation.add_ttl_function(pmt_gate_ttl_channel, ttl_gate)
 sequence.add_segment("excite", segment_excitation)
 
 sequence.setup_sequence([("test", 1), ("excite", 1)])
 
-## setup the awg
-m4i = M4i6622(
-    channelNum=4,
-    sequenceReplay=True,
-    loop_list=sequence.segment_loops,
-    next=sequence.segment_next,
-    conditions=sequence.segment_conditions,
-    initialStep=0,
-    segTimes=sequence.segment_durations,
-    ttlOutOn=sequence.ttl_out_on,
-    ttlOutChannel=sequence._ttl_channels,
-    verbose=True,
-)
-m4i.setSoftwareBuffer()
-m4i.configSequenceReplay(
-    segFunctions=sequence.segment_awg_functions,
-    steps=sequence.segment_steps,
-    ttlFunctionList=sequence.segment_ttl_functions,
-)
+## setup the awg and digitizer
+m4i = M4i6622()
+m4i.setup_sequence(sequence)
 
-## setup the digitizer
-params_dict = {
-    "num_samples": int(measurement_time.to("s").magnitude * sampling_rate),
-    "sampling_rate": int(sampling_rate),
-    "ch1_voltage_range": 4,
-    "ch2_voltage_range": 4,
-    "trigger_channel": 0,
-    "data_format": "float32",
-    "coupling": "DC",
-    "num_records": repeats + 1,
-    "triggers_per_arm": repeats + 1,
-}
-dg.set_parameters(params_dict)
-dg.configure()
-dg.set_two_channel_waitForTrigger()
+dg = DigitizerVisa("192.168.0.125")
+dg.configure_acquisition(
+    sample_rate=sampling_rate,
+    samples_per_record=int(measurement_time.to("s").magnitude * sampling_rate),
+    num_records=repeats,
+)
+dg.configure_channels(channels=[1], voltage_range=2.0)
+dg.set_trigger_source_external()
+dg.set_arm(triggers_per_arm=repeats)
 
 ## take data
 epoch_times = []
 pmt_voltages = []
 
-for kk in range(repeats + 1):
-    m4i.startCard(False)
+for kk in range(repeats):
+    m4i.start_sequence()
     epoch_times.append(time.time())
-    time.sleep(sequence.total_duration + measurement_time.to("s").magnitude)
-    time.sleep(time_between_repeat)
-m4i.stop()
+    m4i.wait_for_sequence_complete()
 
-for kk in range(repeats + 1):
-    V1, _ = dg.get_two_channel_waveform(kk + 1)
-    if kk != 0:
-        pmt_voltages.append(V1)  # first run data is always incorrect.
+m4i.stop_sequence()
 
-pmt_times = [kk / sampling_rate for kk in range(len(V1))]
+pmt_voltages = dg.get_waveforms([1], records=(1, repeats))[0]
+pmt_times = [kk / sampling_rate for kk in range(len(pmt_voltages[0]))]
 
 ## save data
 data = {
