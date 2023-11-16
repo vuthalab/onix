00015ccc86319f131ee6b5ba2a2545b5cfef52b5
Mingyu Fan <mingyufan212@gmail.com>
2023-11-15 21:08:54 -0500

    add crystal number density calculations

---
+++ models/crystal.py
@@ -0,0 +1,19 @@
+import numpy as np
+from onix.units import ureg, Q_
+
+
+class EuYSO:
+    def __init__(self, x: Q_, y: Q_, z: Q_, concentation: float):
+        self.volume = x * y * z
+        self.concentration = concentation
+
+        self._density = 4.44 * ureg.g / ureg.cm**3
+        self._yso_atom_mass = (89 * 2 + 28 + 16 * 5) * ureg.Da
+
+    @property
+    def eu_site1_number_density(self) -> Q_:
+        return (self._density / self._yso_atom_mass * self.concentration).to("mm^-3")
+
+    @property
+    def eu_site1_numbers(self) -> float:
+        return (self.eu_site1_number_density * self.volume).to("").magnitude
