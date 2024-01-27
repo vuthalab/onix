b7f8d0168fab8f56d58c8db554bbaa22ecc95c03
Vutha lab @ UofT <electric.atoms@gmail.com>
2024-01-26 19:42:51 -0500

    fixed get data issue

--- micro/pid_controller/pid_controller.ino
+++ micro/pid_controller/pid_controller.ino
@@ -12,55 +12,61 @@ const uint8_t CONTROL_OUTPUT = 1;
 // interval in us for ADC data reading.
 // Shorter inverval will cause the program to freeze due to too many ADC reading requests.
 // Longer interval slow down the PID loop.
-const uint16_t ADC_INTERVAL = 3;
+const uint16_t ADC_INTERVAL = 2;
 const uint16_t ADC_DELAY = 0;
 const adc_scale_t ADC_SCALE = BIPOLAR_2500mV;
 
-double p_gain = 0.1;
-double i_time = 900.0;
-double d_time = 0.0;
+float p_gain = 0.1;
+float i_time = 900.0;
+float d_time = 0.0;
 
 // keeps track of the integral term
-double integral = 0.0;
+float integral = 0.0;
 // limits the integral term magnitude so it does not blow up
-double integral_limit = 10.0;
+float integral_limit = 10.0;
 // last error signal for D gain.
-double previous_error = 0.0;
+float current_error = 0.0;
+float previous_error = 0.0;
 
 // error signal offset before going into the PID loop
-double error_offset = 1.5;
+float error_offset = 1.5;
 
 // output voltage offset
-double output_offset = 0.0;
+float output_offset = 0.0;
 // output voltage limits
-double output_lower_limit = 0.0;
-double output_upper_limit = 10.0;
+float output_lower_limit = 0.0;
+float output_upper_limit = 10.0;
 
 // PID loop running or not
 bool pid_state = false;
 
 // Data saved in Quarto for computer readout
-const int DATA_LENGTH = 1000;
-double error_data[DATA_LENGTH];
+const int DATA_LENGTH = 50000;
+float error_data[DATA_LENGTH];
 int error_index = 0;
-double output_data[DATA_LENGTH];
+bool stop_error_data = false;
+float output_data[DATA_LENGTH];
 int output_index = 0;
+bool stop_output_data = false;
 
 
 void adc_loop(void) {
-  error_data[error_index] = readADC1_from_ISR();
-  if (error_index < DATA_LENGTH - 1) {
-    error_index++;
-  }
-  else {
-    error_index = 0;
+  current_error = readADC1_from_ISR();
+  if (!stop_error_data) {
+    error_data[error_index] = current_error;
+    if (error_index < DATA_LENGTH - 1) {
+      error_index++;
+    }
+    else {
+      error_index = 0;
+    }
   }
   update_pid();  // this function must be fast compared to the ADC sample interval.
 }
 
-double new_integral(double integral, double integral_change) {
+float new_integral(float integral, float integral_change) {
   // limit the integral term to be within the limit.
-  double pending_integral = integral + integral_change;
+  float pending_integral = integral + integral_change;
   if (pending_integral > integral_limit) {
     return integral_limit;
   }
@@ -74,19 +80,13 @@ double new_integral(double integral, double integral_change) {
 
 void update_pid(void) {
   // takes <1 us to finish. Safe to put it in the ADC loop with ~3 us interval.
-  double output = output_offset;
+  float output = output_offset;
   if (pid_state) {
-    double error = 0.0;
-    if (error_index > 0) {
-      error = error_data[error_index - 1] - error_offset;
-    }
-    else {
-      error = error_data[DATA_LENGTH - 1] - error_offset;
-    }
-    double proportional = p_gain * error;
+    float error = current_error - error_offset;
+    float proportional = p_gain * error;
     integral = new_integral(integral, p_gain / i_time * error);
-    double differential = p_gain * d_time * (error - previous_error);
-    previous_error = error;
+    float differential = p_gain * d_time * (current_error - previous_error);
+    previous_error = error + error_offset;
 
     output += proportional + integral + differential;
     if (output > output_upper_limit) {
@@ -98,12 +98,14 @@ void update_pid(void) {
   }
   writeDAC(CONTROL_OUTPUT, output);
 
-  output_data[output_index] = output;
-  if (output_index < DATA_LENGTH - 1) {
-    output_index++;
-  }
-  else {
-    output_index = 0;
+  if (!stop_output_data) {
+    output_data[output_index] = output;
+    if (output_index < DATA_LENGTH - 1) {
+      output_index++;
+    }
+    else {
+      output_index = 0;
+    }
   }
 }
 
@@ -174,21 +176,25 @@ void cmd_pid_state(qCommand& qC, Stream& S){
 }
 
 void cmd_error_data(qCommand& qC, Stream& S){
+  stop_error_data = true; // pause in data taking during process
   for (int i = error_index; i < DATA_LENGTH; i++) {
     S.printf("%f\n", error_data[i]);
   }
   for (int i = 0; i < error_index; i++) {
     S.printf("%f\n", error_data[i]);
   }
+  stop_error_data = false;
 }
 
 void cmd_output_data(qCommand& qC, Stream& S){
+  stop_output_data = true; // pause in data taking during process
   for (int i = output_index; i < DATA_LENGTH; i++) {
     S.printf("%f\n", output_data[i]);
   }
   for (int i = 0; i < output_index; i++) {
     S.printf("%f\n", output_data[i]);
   }
+  stop_output_data = false;
 }
 
 void setup(void) {
