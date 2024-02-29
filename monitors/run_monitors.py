3088292eb712c621017ceb7f9ce3576e68aa455f
Vutha lab @ UofT <electric.atoms@gmail.com>
2024-02-29 17:33:11 -0500

    Update run_monitors.py

--- monitors/run_monitors.py
+++ monitors/run_monitors.py
@@ -10,7 +10,8 @@ from onix.headers.ctc100 import CTC100
 token = os.environ.get("INFLUXDB_TOKEN")
 org = "onix"
 url = "http://onix-pc:8086"
-bucket="permanent"
+bucket_permanent = "permanent"
+bucket_live = "live"
 
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = write_client.write_api(write_options=SYNCHRONOUS)
@@ -20,6 +21,10 @@ wm = WM()
 c = CTC100("192.168.0.202")
 channels = c.channels
 
+counter = 0
+high_freq_time = 10
+low_freq_time = 300
+
 while True:
     try:
         point = Point("pulse_tube")
@@ -27,7 +32,9 @@ while True:
         point.field("state", state)
         for kk in pt.variables:
             point.field(kk, pt.variables[kk][1])
-        write_api.write(bucket=bucket, org="onix", record=point)
+        write_api.write(bucket=bucket_live, org="onix", record=point)
+        if counter == low_freq_time:
+            write_api.write(bucket=bucket_permanent, org="onix", record=point)
     except:
         print("Pulse tube error.")
 
@@ -37,7 +44,9 @@ while True:
         if isinstance(freq, str):
             freq = -1
         point.field("frequency", freq)
-        write_api.write(bucket=bucket, org="onix", record=point)
+        write_api.write(bucket=bucket_live, org="onix", record=point)
+        if counter == low_freq_time:
+            write_api.write(bucket=bucket_permanent, org="onix", record=point)
     except:
         print("Wavemeter error.")
 
@@ -46,8 +55,13 @@ while True:
         for channel in channels:
             value = c.read(channel)
             point.field(channel, value)
-        write_api.write(bucket=bucket, org="onix", record=point)
+        write_api.write(bucket=bucket_live, org="onix", record=point)
+        if counter == low_freq_time:
+            write_api.write(bucket=bucket_permanent, org="onix", record=point)
     except:
         print("CTC100 error.")
 
-    time.sleep(300)
+    if counter == low_freq_time:
+        counter = 0
+    counter += high_freq_time
+    time.sleep(high_freq_time)
