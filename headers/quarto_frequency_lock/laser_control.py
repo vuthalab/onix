e76be423c2d8a471111bffc047e1835e7591d9da
Vutha lab @ UofT <electric.atoms@gmail.com>
2024-03-11 16:48:32 -0400

    track rms error

--- headers/quarto_frequency_lock/laser_control.py
+++ headers/quarto_frequency_lock/laser_control.py
@@ -1,16 +1,16 @@
+import os
+import numpy as np
+import threading
 from onix.headers.quarto_frequency_lock import Quarto
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtWidgets
-import threading
 from PyQt5.QtWidgets import *
-import os
 import influxdb_client
 from influxdb_client import Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 app = pg.mkQApp("Laser control")
 q = Quarto("/dev/ttyACM3")
-
 device_lock = threading.Lock()
 
 win = pg.GraphicsLayoutWidget(show=True, title="")
@@ -19,7 +19,7 @@ pg.setConfigOptions(antialias=True)
 
 LENGTH = 1000
 
-p_error = win.addPlot(title="error signal", colspan = 3)
+p_error = win.addPlot(title="error signal", colspan = 4)
 p_error.setMouseEnabled(x=False)
 error = p_error.plot(pen='y')
 def update_p_error(data):
@@ -27,7 +27,7 @@ def update_p_error(data):
     error.setData(data)
 win.nextRow()
 
-p_output = win.addPlot(title="output signal", colspan = 3)
+p_output = win.addPlot(title="output signal", colspan = 4)
 p_output.setMouseEnabled(x=False)
 output = p_output.plot(pen='y')
 def update_p_output(data):
@@ -35,7 +35,7 @@ def update_p_output(data):
     output.setData(data)
 win.nextRow()
 
-p_transmission = win.addPlot(title="transmission signal", colspan = 3)
+p_transmission = win.addPlot(title="transmission signal", colspan = 4)
 p_transmission.setMouseEnabled(x=False)
 transmission = p_transmission.plot(pen='y')
 def update_p_transmission(data):
@@ -43,7 +43,7 @@ def update_p_transmission(data):
     transmission.setData(data)
 win.nextRow()
 
-p_cavity_error = win.addPlot(title="cavity error signal", colspan = 3)
+p_cavity_error = win.addPlot(title="cavity error signal", colspan = 4)
 p_cavity_error.setMouseEnabled(x=False)
 cavity_error = p_cavity_error.plot(pen='y')
 def update_p_cavity_error(data):
@@ -53,6 +53,7 @@ win.nextRow()
 
 plots = True
 def update_all():
+    global rms_err
     if plots == True:
         with device_lock:
             data = q.get_all_data()
@@ -67,6 +68,7 @@ timer = QtCore.QTimer()
 timer.timeout.connect(update_all)
 timer.start(50)
 
+## Lock On / Off Button
 def on_button_pressed():
     if lock_state.text() == "Lock On" or lock_state.text() == "Autorelock On":
         lock_state.setText("Lock Off")
@@ -95,15 +97,16 @@ elif initial_lock_state == 2:
 lock_state.clicked.connect(on_button_pressed)
 lock_state_proxy = QtWidgets.QGraphicsProxyWidget()
 lock_state_proxy.setWidget(lock_state)
-win.addItem(lock_state_proxy, row = 5, col = 0)
+win.addItem(lock_state_proxy, row = 5, col = 0, colspan = 2)
 
+## Output Offset Spinbox
 def _offset():
     with device_lock:
         q.set_output_offset(offset.value())
 
 with device_lock:
     initial_offset = q.get_output_offset()
-offset = QtWidgets.QDoubleSpinBox(prefix = "Offset: ")
+offset = QtWidgets.QDoubleSpinBox(prefix = "Offset: ", suffix = " V")
 offset.setValue(initial_offset)
 offset.setDecimals(2)
 offset.setSingleStep(0.01)
@@ -112,13 +115,14 @@ offset.setMinimum(-10)
 offset.setMaximum(10)
 offset_proxy = QtWidgets.QGraphicsProxyWidget()
 offset_proxy.setWidget(offset)
-win.addItem(offset_proxy, row = 5, col = 1)
+win.addItem(offset_proxy, row = 5, col = 2)
 
+## Scan Spinbox
 def _scan():
     with device_lock:
         q.set_scan(scan.value())
 
-scan = QtWidgets.QDoubleSpinBox(prefix = "Scan: ")
+scan = QtWidgets.QDoubleSpinBox(prefix = "Scan: ", suffix = " V")
 with device_lock:
     initial_scan = q.get_scan()
 scan.setValue(initial_scan)
@@ -129,9 +133,10 @@ scan.setMinimum(0)
 scan.setMaximum(10)
 scan_proxy = QtWidgets.QGraphicsProxyWidget()
 scan_proxy.setWidget(scan)
-win.addItem(scan_proxy, row = 5, col = 2)
+win.addItem(scan_proxy, row = 5, col = 3)
 win.nextRow()
 
+## Integral and Output Warnings
 def update_warning():
     with device_lock:
         state = q.get_state()
@@ -159,6 +164,7 @@ warning_timer = QtCore.QTimer()
 warning_timer.timeout.connect(update_warning)
 warning_timer.start(5000)
 
+## Stop Plots Button
 def stop_plots_pressed():
     global plots
     if plots == True:
@@ -174,10 +180,17 @@ stop_plots_proxy = QtWidgets.QGraphicsProxyWidget()
 stop_plots_proxy.setWidget(stop_plots)
 win.addItem(stop_plots_proxy, row = 6, col = 1)
 
+## Transmission
+def round_sig(x, sig=3):
+    """
+    Rounds to specified number of significant figures
+    """
+    return round(x, sig-int(np.floor(np.log10(abs(x))))-1)
+
 def update_transmission():
     with device_lock:
         val = q.get_last_transmission_point()
-    last_transmission.setText(f"Transmission: {round(val,2)}")
+    last_transmission.setText(f"Transmission: {round_sig(val,3)}")
 
 last_transmission = QtWidgets.QPushButton()
 last_transmission_proxy = QtWidgets.QGraphicsProxyWidget()
@@ -189,6 +202,33 @@ last_transmission_timer = QtCore.QTimer()
 last_transmission_timer.timeout.connect(update_transmission)
 last_transmission_timer.start(1000)
 
+## RMS Error
+track_rms_err = True
+
+def _toggle_rms_error():
+    global track_rms_err
+    if rms_err.text == "RMS Error: --":
+        track_rms_err = True
+    else: 
+        track_rms_err = False
+        rms_err.setText("RMS Error: --")
+
+def update_rms_err():
+    if track_rms_err == True:
+        with device_lock:
+            err_data = q.get_error_data()
+        err_squared = [i**2 for i in err_data]
+        rms_err = sum(err_squared) / np.sqrt(len(err_squared))
+        rms_err.setText(f"RMS Error: {round_sig(rms_err,3)} V")
+    else: 
+        pass
+
+rms_err = QtWidgets.QPushButton()
+rms_err_proxy = QtWidgets.QGraphicsProxyWidget()
+rms_err_proxy.setWidget(rms_err)
+stop_plots.clicked.connect(_toggle_rms_error)
+update_rms_err()
+win.addItem(rms_err_proxy, row = 6, col = 3)
 
 ## Monitor the integral and output using influx db
 token = os.environ.get("INFLUXDB_TOKEN")
