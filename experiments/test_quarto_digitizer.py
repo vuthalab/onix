d8981b72f5c8acfe5bebf71515fac4c8a3542ba1
Vutha lab @ UofT <electric.atoms@gmail.com>
2024-04-12 16:43:33 -0400

    Update test_quarto_digitizer.py

--- experiments/test_quarto_digitizer.py
+++ experiments/test_quarto_digitizer.py
@@ -1,108 +1,74 @@
 import time
-import os
-import numpy as np
 
 from onix.data_tools import save_experiment_data
 from onix.units import ureg
 
-from onix.experiments.shared import m4i, run_sequence
+from onix.experiments.shared import m4i
 from onix.sequences.sequence import Sequence, Segment, AWGSinePulse, TTLPulses
+from onix.headers.quarto_digitizer import Quarto
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+try:
+    q
+except Exception:
+    q = Quarto("/dev/ttyACM6")
+
+## params
+params = {
+    "awg_channel": 4,
+    "ttl_channel": 3,
+    "frequency": 100 * ureg.kHz,
+    "amplitude": 20000,
+    "duration": 20 * ureg.us,
+    "delay_time": 4 * ureg.us,
+    "trigger_offset": 0 * ureg.us,
+    "trigger_duration": 1 * ureg.us,
+    "repeats": 10,
+}
 
 ## setup sequence
 sequence = Sequence()
 
-segment_
-segment_test = Segment("test")
-offset = 100 * ureg.us
-ttl_gate = TTLPulses([[0, offset]]) # ttl 0-100us
-segment_test.add_ttl_function(pmt_gate_ttl_channel, ttl_gate)
-pulse_test = AWGSinePulse( # awg 210 us - 220 us
-    frequency=test_aom_frequency,
-    amplitude=test_aom_amplitude,
-    start_time=offset*2+excitation_delay,
-    end_time=offset*2+excitation_delay+test_time,
-)
-segment_test.add_awg_function(excitation_aom_channel, pulse_test)
-segment_test._duration = segment_test.duration + offset
-sequence.add_segment(segment_test)
-
-segment_excitation = Segment("excite")
-pulse_excitation = AWGSinePulse( # awg 10 us - 5ms
-    frequency=excitation_aom_frequency,
-    amplitude=excitation_aom_amplitude,
-    start_time=excitation_delay,
-    end_time=excitation_delay+excitation_time,
-) # ttl 5 ms + 20us - 12 ms + 20us
-segment_excitation.add_awg_function(excitation_aom_channel, pulse_excitation)
-ttl_gate = TTLPulses([[0, 2 * excitation_delay + excitation_time]])
-segment_excitation.add_ttl_function(pmt_gate_ttl_channel, ttl_gate)
-sequence.add_segment(segment_excitation)
-
-sequence.setup_sequence([("test", 1), ("excite", 1)])
+segment = Segment("main")
+segment.add_ttl_function(params["ttl_channel"], TTLPulses([[params["trigger_offset"], params["trigger_offset"] + params["trigger_duration"]]]))
+segment.add_awg_function(params["awg_channel"], AWGSinePulse(params["frequency"], params["amplitude"], end_time=params["duration"]))
+segment._duration = segment.duration + params["delay_time"]
+sequence.add_segment(segment)
+sequence.setup_sequence([("main", params["repeats"])])
 
 ## setup the awg and digitizer
 m4i.setup_sequence(sequence)
-
-duration = measurement_time.to("s").magnitude
-dg.set_acquisition_config(
-    num_channels=1,
-    sample_rate=sampling_rate,
-    segment_size=int(duration * sampling_rate),
-    segment_count=repeats,
-)
-
-dg.set_channel_config(channel=1, range=5, high_impedance=False)
-dg.set_trigger_source_edge()
-dg.write_configs_to_device()
+sample_size = int(params["duration"].to("us").magnitude)
+sample_count = int(params["repeats"])
+q.setup(sample_size, sample_count)
 
 ## take data
-epoch_times = []
-pmt_voltages = []
-pmt_times = []
-
-# dg.initiate_data_acquisition()
-dg.start_capture()
-
-for kk in range(repeats):
-    time.sleep(0.1)
-    m4i.start_sequence()
-    epoch_times.append(time.time())
-    m4i.wait_for_sequence_complete()
-    time.sleep(time_between_repeat)
-
+q.start()
+m4i.start_sequence()
+m4i.wait_for_sequence_complete()
 m4i.stop_sequence()
-timeout = 1
-dg.wait_for_data_ready(timeout)
-sample_rate, digitizer_data = dg.get_data()
-pmt_voltages = digitizer_data[0]
-# pmt_voltages = np.average(pmt_voltages, axis=0)
-pmt_times = [kk / sample_rate for kk in range(len(pmt_voltages[0]))]
+q.stop()
+data = q.data().reshape((sample_count, sample_size))
+times = np.arange(sample_size)
+
+## plot data
+for kk, d in enumerate(data):
+    plt.plot(times - kk * 0.013, data[kk])
+plt.ylabel("Voltage (V)")
+plt.xlabel("Time (us)")
+plt.show()
 
 ## save data
 header = {
-    "excitation_aom_channel": excitation_aom_channel,
-    "excitation_aom_frequency": excitation_aom_frequency,
-    "excitation_aom_amplitude": excitation_aom_amplitude,
-    "excitation_time": excitation_time,
-    "excitation_delay": excitation_delay,
-    "test_aom_channel": test_aom_channel,
-    "test_aom_frequency": test_aom_frequency,
-    "test_aom_amplitude": test_aom_amplitude,
-    "pmt_gate_ttl_channel": pmt_gate_ttl_channel,
-    "measurement_time": measurement_time,
-    "repeats": repeats,
-    "time_between_repeat": time_between_repeat,
-    "sampling_rate": sample_rate,
-    "frequency": frequency,
+    "params": params,
 }
 data = {
-    "pmt_times": pmt_times,
-    "pmt_voltages": pmt_voltages,
-    "epoch_times": epoch_times,
+    "data": data,
 }
-name = "Fluorescence Decay"
-data_id = save_experiment_data(name, data, header)
-print(data_id)
+name = "Quarto digitizer test"
+#data_id = save_experiment_data(name, data, header)
+#print(data_id)
 
 ##
