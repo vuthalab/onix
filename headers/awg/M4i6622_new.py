f256a3134c11d8f3b0a36ed9a729bf4de2a1b679
Shravan <90870302+zipstyx@users.noreply.github.com>
2024-05-26 14:28:28 -0400

    update

--- headers/awg/M4i6622_new.py
+++ headers/awg/M4i6622_new.py
@@ -68,7 +68,7 @@ class M4i6622:
     def __init__(
         self,
         addresses: Union[list[str], str] = ["/dev/spcm0", "/dev/spcm1"],
-        external_clock_frequency: Optional[int] = None
+        external_clock_frequency: Optional[int] = None,
     ):
         if not isinstance(addresses, list):
             addresses = [addresses]
@@ -88,22 +88,16 @@ class M4i6622:
             self._reset(hcard)
         self._bytes_per_sample = self._get_bytes_per_sample(self._hcards[0])
 
-        """
-        Testing clock sharing by setting an external clock frequency
-        for the second AWG and running the internal clock on the first
-        one. Assuming that spcm0 corresponds to channels 0-3 and
-        spcm1 to 4-7.
-        """
-        if addresses == ["/dev/spcm0", "/dev/spcm1"]:
-            ext_clock_freq_2 = 10000000
-            self._set_clock_mode(self._hcards[0], "internal_pll")
-            self._set_external_clock_mode(self._hcards[0], "enable")
-            # print(self._get_sample_rate(self._hcards[0]))
-            self._set_clock_mode(self._hcards[1], "internal_pll") #external_reference pxi_reference internal_pll
-            self._set_external_clock_frequency(
-                self._hcards[1], ext_clock_freq_2)
+        if True:    # new code
+            self._set_clock_mode(self._hcards[0], 'internal_pll')
+            self._set_clock_mode(self._hcards[1], 'external_reference')
 
-        else:   # old code contained in this else statement
+            self._enable_clkout(self._hcards[0], int(50e6))
+            clock_freq = pyspcm.dwGetParam_i32(
+                self._hcards[0], pyspcm.SPC_REFERENCECLOCK)
+            self._set_external_clock_frequency(self._hcards[1], clock_freq)
+
+        else:   # old code
             if external_clock_frequency is not None:
                 for hcard in self._hcards:
                     self._set_clock_mode(hcard, "external_reference")
@@ -140,9 +134,7 @@ class M4i6622:
         # Maps of TTL channels mixing with AWG channels
         self._ttl_awg_map = {0: 0, 1: 1, 2: 2}
 
-
         for hcard in self._hcards:
-
             for ttl_channel in [0, 1, 2]:
                 mode = pyspcm.SPCM_XMODE_DIGOUT
 
@@ -171,6 +163,31 @@ class M4i6622:
         self._sine_segment_running = False
         self._set_sine_segment()
         self.setup_sequence(Sequence())
+
+    # New function
+    def _enable_clkout(self, hcard):
+        # Open the device
+
+        if hcard == 0:
+            print("Error opening device")
+            exit(1)
+
+        # Set the CLKOUT frequency - not possible because the
+        """
+        ret = pyspcm.SPCM_dwSetParam_i32(
+            hcard, pyspcm.SPC_CLOCKOUTFREQUENCY, clkout_frequency)
+        if ret != 0:
+            print(f"Error setting CLKOUT frequency: {ret}")
+        """
+
+        # Enable the CLKOUT port
+        ret = pyspcm.SPCM_dwSetParam_i32(
+            hcard, pyspcm.SPC_CLOCKOUT, 1)
+        if ret != 0:
+            print(f"Error enabling CLKOUT: {ret}")
+        else:
+            print("CLKOUT enabled with frequency:", pyspcm.SPCM_dwGetParam_i32(
+                hcard, pyspcm.SPC_CLOCKOUTFREQUENCY))
 
     # device methods
     def _get_bytes_per_sample(self, hcard) -> int:
@@ -407,21 +424,6 @@ class M4i6622:
         if ret != pyspcm.ERR_OK:
             raise Exception(f"Set number of loops failed with code {ret}.")
 
-    def _set_external_clock_mode(
-            self,
-            hcard,
-            external_clock_mode: Literal[
-                "enable"
-            ],
-    ):
-        if external_clock_mode == "enable":
-            value = 1
-        else:
-            raise ValueError(f"External clock mode {external_clock_mode} is not valid.")
-        ret = pyspcm.spcm_dwSetParam_i32(hcard, pyspcm.SPC_CLOCKOUT, value)
-        if ret != pyspcm.ERR_OK:
-            raise Exception(f"Set external clock mode failed with code {ret}.")
-        
     def _set_clock_mode(
         self,
         hcard,
