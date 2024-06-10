df1ccb4d2743855f6e96abf250880852b6306b0f
Vutha lab @ UofT <electric.atoms@gmail.com>
2024-06-10 19:44:42 -0400

    update

--- headers/microphone/amar.py
+++ headers/microphone/amar.py
@@ -1,9 +1,12 @@
 from onix.headers.microphone import Quarto
+from onix.analysis.fitter import Fitter
+
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import butter, sosfilt
-from scipy.signal.windows import boxcar
 import time
+
+from pprint import pprint
+
 from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
 
@@ -15,10 +18,18 @@ adc_interval = q.adc_interval   # [s]
 sample_rate = 1/adc_interval    # [samples per second]
 
 data = np.array([])
-fig, (ax1, ax2) = plt.subplots(2, 1)
 
-for i in range(20): 
+
+# TODO: does this transfer duplicate values?
+for i in range(10): 
     data = np.append(data, q.data())
+    time.sleep(1e-6*30000)
+
+# np.abs(data)
+
+plt.plot(data)
+plt.show()
+print(data.size)
 
 ## windowed average
 N = 5000
@@ -34,36 +45,61 @@ times = times[times>0.5]
 max_noise_freq = 5  # [Hz]
 peak_distance = int(sample_rate/max_noise_freq)    # checking for peaks with frequency <= 5 Hz
 peaks, properties = find_peaks(avgs, height=np.max(avgs)-0.001, distance=peak_distance)
-average_time_dif_bw_peaks =  np.mean(np.abs(times[peaks] - np.roll(times[peaks], 1))[1:])
-freq_estimate_from_peaks = 1/average_time_dif_bw_peaks
+time_dif_bw_peaks =  np.abs(times[peaks] - np.roll(times[peaks], 1))[1:]
+freq_estimate_from_peaks = 1/np.mean(time_dif_bw_peaks)
 
+print(f'Frequency estimate from peaks = {freq_estimate_from_peaks} Hz from {time_dif_bw_peaks.size} samples')
+fig, (ax1, ax2) = plt.subplots(2, 1)
 ax1.plot(times, avgs, label='avg', color='black')
-ax1.set_xlabel('time (us)')
+ax1.set_xlabel('time (s)')
 ax1.set_ylabel('signal with mean set to 0 (V)')
 ax1.scatter(times[peaks], avgs[peaks], color='red')
 ax1.set_title('averaged signal')
 
 ## FFT
-
+fig, (ax1, ax2) = plt.subplots(2, 1)
 avgs_fft = np.fft.rfft(avgs)
 fft_freqs = np.fft.rfftfreq(avgs.size, 10e-6)
 
 freq_peak = fft_freqs[np.argmax(avgs_fft)]
+print(f'FFT peak frequency = {freq_peak} Hz')
 
-ax2.plot(fft_freqs, np.abs(avgs_fft), label='fft')
+ax2.loglog(fft_freqs, np.abs(avgs_fft), label='fft')
 ax2.set_title('Fourier Transform of averaged signal')
 ax2.set_xlabel('frequency (Hz)')
 ax2.set_ylabel('signal with time domain \n mean set to 0 (V)')
 
-## curvefit
-print(f'FFT peak frequency = {freq_peak} Hz')
-print(f'Frequency estimate from peaks = {freq_estimate_from_peaks} Hz')
-p0=(freq_estimate_from_peaks, np.max(avgs), np.pi/2 - 2*np.pi*freq_estimate_from_peaks*times[peaks[0]], np.mean(avgs))
-
-phi_pred = np.pi/2 - 2*np.pi*freq_estimate_from_peaks*times[peaks[0]]
-
-popt, pcov = curve_fit(sin, times, avgs, p0=p0)
-print(f"(f, A, phi, c)= {popt}")
-ax1.plot(times, sin(times, *p0), label='curvefit')
-
 plt.show()
+
+## curvefit
+phi_pred0 = np.pi/2 - 2*np.pi*freq_estimate_from_peaks*times[peaks[0]]
+phi_pred_acc = np.angle(np.exp(1j*phi_pred0))
+p0= {'f': freq_estimate_from_peaks, 'A': np.max(avgs), 'phi':  phi_pred_acc, 'c': np.mean(avgs)}
+bounds = {'f': (0, 5), 'A': (0, np.max(avgs)), 'phi': (-np.pi, np.pi), 'c': (-np.fabs(np.mean(avgs)), np.fabs(np.mean(avgs)))}
+
+# between 3.34 and 3.35 Hz seems like the usual frequency
+
+if time_dif_bw_peaks.size >= 5: #TODO: remove magic number
+    fitter = Fitter(sin)
+    fitter.set_data(times, avgs)
+    fitter.set_p0(p0)
+
+    for param in bounds.keys():
+        fitter.set_bounds(param, *bounds[param])
+
+    fitter.fit()
+    popt = fitter.results
+
+    # popt, pcov = curve_fit(sin, times, avgs, p0=tuple(p0.values()))
+    print(f"(f, A, phi, c)= {popt}")
+    fig, ax1 = plt.subplots(1, 1)
+    ax1.plot(times, sin(times, **p0), label='curvefit')
+    ax1.plot(times, avgs, label='avg', color='black')
+    ax1.set_xlabel('time (s)')
+    ax1.set_ylabel('signal with mean set to 0 (V)')
+    ax1.scatter(times[peaks], avgs[peaks], color='red')
+    ax1.set_title('averaged signal')
+
+    ax1.legend()
+
+    plt.show()
