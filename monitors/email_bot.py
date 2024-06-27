792d42798b0fcb531561db333d0c17d8e5077bed
Vutha lab @ UofT <electric.atoms@gmail.com>
2024-06-27 19:19:10 -0400

    email bot monitors code errors

--- monitors/email_bot.py
+++ monitors/email_bot.py
@@ -24,6 +24,7 @@ https://influxdb-client.readthedocs.io/en/latest/api.html
 """
 
 recipients = ["alek.radak@mail.utoronto.ca", "amar.vutha@utoronto.ca", "mingyufan212@gmail.com", "bassam.nima@mail.utoronto.ca"]
+
 high_transmission_level = 0.3 # V; what transmission level indicates the lock is on
 instantaneous_lock_delta_t = 5 # check the lock every 5 seconds
 long_term_lock_delta_t = 60 * 5 # long term behavior of the lock is determined by its lock state over the last 5 minutes
@@ -38,6 +39,15 @@ def send_email(lockstate):
     elif lockstate == "relock":
         subject = f"[Lock Alert] Relock at {time}"
         message = f"Relock at {time}"
+    elif lockstate == "unstable":
+        subject = f"[Lock Alert] Unstable at {time}"
+        message = f"Unstable at {time}"
+    elif lockstate == "code error":
+        subject = f"[Lock Alert] Code broke at {time}"
+        message = f"Code broke at {time}"
+    elif lockstate == "code fixed":
+        subject = f"[Lock Alert] Code fixed at {time}"
+        message = f"Code is no longer broken at {time}"
 
     msg = MIMEMultipart()
     msg['From'] = 'onix.toronto@gmail.com'
@@ -52,31 +62,38 @@ def send_email(lockstate):
     mailserver.login('onix.toronto@gmail.com', 'pmnk chkz ptdx rnlv')
     mailserver.sendmail('onix.toronto@gmail.com',recipients ,msg.as_string())
     mailserver.quit()
+    print(f'Email {lockstate} sent.')
 
 def check_instantaneous_lock(): 
     """
-    Query indluxdb for the most recent transmission point. Returns 1 if high and 0 is low.
+    Query indluxdb for the most recent transmission point. First argument is 1 if high and 0 is low.
+    Second argument is 0 if we got the data from influx without issue. This returns 1 if there was some error.
     """
-    token = os.environ.get("INFLUXDB_TOKEN")
-    org = "onix"
-    url = "http://onix-pc:8086"
+    try:
+        token = os.environ.get("INFLUXDB_TOKEN")
+        org = "onix"
+        url = "http://onix-pc:8086"
 
-    query_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-    query_api = query_client.query_api()
+        query_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+        query_api = query_client.query_api()
 
-    tables = query_api.query(
-    (
-        'from(bucket:"week") |> range(start: -5s) '
-        '|> filter(fn: (r) => r["_measurement"] == "laser_controller")'
-        '|> filter(fn: (r) => r["_field"] == "transmission")'
-    )
-    )
-    values =  np.array([record["_value"] for table in tables for record in table.records])
-    transmission =  values[-1]
+        tables = query_api.query(
+        (
+            'from(bucket:"week") |> range(start: -20s) '
+            '|> filter(fn: (r) => r["_measurement"] == "laser_controller")'
+            '|> filter(fn: (r) => r["_field"] == "transmission")'
+        )
+        )
+        values =  np.array([record["_value"] for table in tables for record in table.records])
+    
+        transmission =  values[-1]
+    except: # if there is some error in getting the transmission then return the average value, but keep track of errors
+        return np.mean(recent_lock_history), 1
+    
     if transmission > high_transmission_level:
-        return 1
+        return 1, 0
     else:
-        return 0
+        return 0, 0
 
 def check_long_term_lock():
     if np.mean(recent_lock_history) == 1: # if lock has been on the last 5 minutes, lock_state = 1
@@ -85,32 +102,54 @@ def check_long_term_lock():
         return 0
     else:
        return 2 # if lock has not been stable the last 5 minutes, lock state = 2
+    
+def check_error_state():
+    if np.mean(errors_list) == 1: # code is fully broken over the last five minutes
+        return 1
+    elif np.mean(errors_list) == 0: # code is fully working over the last five minutes
+        return 0
+    else:
+        return 2 # code has been unstable over last five minutes
 
+errors_list = []
 recent_lock_history = []
 i = 0
 
 while True:
     if len(recent_lock_history) < N:
         start_lock_state = None
+        start_error_state = None
     else:
         start_lock_state = check_long_term_lock()
+        start_error_state = check_error_state()
        
+    state, error = check_instantaneous_lock()
     if len(recent_lock_history) < N:
-        recent_lock_history.append(check_instantaneous_lock())
+        recent_lock_history.append(state)
+        errors_list.append(error)
     else:
-        recent_lock_history[i] = check_instantaneous_lock()
+        recent_lock_history[i] = state
+        errors_list[i] = error
 
     if len(recent_lock_history) < N:
         end_lock_state = None
+        end_error_state = None
     else:
        end_lock_state = check_long_term_lock()
+       end_error_state = check_error_state()
 
     if start_lock_state != None and end_lock_state != None:
         if start_lock_state != end_lock_state and end_lock_state == 0:
-            send_email("broke")
+            send_email("broke") 
         elif start_lock_state != end_lock_state and end_lock_state == 1:
             send_email("relock")
-            pass
+        elif start_lock_state != end_lock_state and end_lock_state == 2:
+            send_email("unstable")
+    if start_error_state != None and end_error_state != None:
+        if start_error_state != end_error_state and end_error_state == 1:
+            send_email("code error") 
+        elif start_error_state != end_error_state and end_error_state == 0:
+            send_email("code fixed")
 
     if i < N-1:
         i = i+1
