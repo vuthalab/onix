64fe96d765e30d654ece94232651d2bac4c6a947
Vutha lab @ UofT <electric.atoms@gmail.com>
2024-08-01 17:15:27 -0400

    Update pulse_sequence_visualizer.py
    PSV Overhaul

--- experiments/pulse_sequence_visualizer.py
+++ experiments/pulse_sequence_visualizer.py
@@ -35,63 +35,89 @@ def contrast(signal):
        return c_sig
 
 
-def plot(raw_data, vis='schem', scale="int"):
+def plot(raw_data:int|list, scale="int"):
        """
-
+       :param raw_data: Trial Number, or list of events
        :param seq: Sequence of lasers fired
-       :param vis: Visualisation method ('schem' or 'matplot')
        :param scale: Size of events ('int', 'log', 'real')
        :return: Plot of the Pulse Sequence
        """
        if type(raw_data) is int:
-              data, headers = get_experiment_data(raw_data)
+              _, headers = get_experiment_data(raw_data)
+              seq = headers["params"]["sequence"]["sequence"]
+              efield = headers["params"]["field_plate"]["use"]
        else:
-              headers = raw_data
-       seq = headers["params"]["sequence"]["sequence"]
-       times = t_scale(params)
-       plots = ['op', 'rf', 'lf']  # , 'ef']
+              seq = raw_data
+              efield = False
+       
+       times = t_scale(headers['params'])
        t_dict = {"op":times[0], "de":times[1], "rf":times[2], "lf":times[3]}
-       names = ['580nm laser', 'RF', 'LF']  # , 'Electric Field']
+       plots = ['op', 'rf', 'lf'] 
+       ef = [[], [], []]
 
-       if vis == 'schem':
-              tdig = {i:[[],[0], []] for i in plots}
-              running_total = 0
-              tags = []
-              for evnt in seq:
-                     # Time scale display
-                     channel = evnt[0][:2]
+       tdig = {i:[[],[0], []] for i in plots}
+       running_total = 0
+       tags = []
+       for evnt in seq:
+              # Time scale display
+              channel = evnt[0][:2] 
+              if channel in ['op', 'de', 'rf', 'lf']:
                      cycle_len = t_dict[channel]
                      if scale == 'real':
-                            evtime = evnt[1]*cycle_len
+                            evtime, tiny_step = evnt[1]*cycle_len, headers["params"]["field_plate"]["ramp_time"].to("ms")
                      elif scale == "int":
-                            evtime = 2
+                            evtime, tiny_step = 1.5, 1
                      elif scale == "log":
-                            evtime = np.log(evnt[1]*cycle_len)/5 + 1
+                            evtime, tiny_step = np.log(evnt[1]*cycle_len) + 1, 1       
 
                      if channel == "de":
                             channel = 'op'
+                     
+                     if efield:
+                            ef_t = False
+                            for etype in headers["params"]["field_plate"]["during"]:
+                                   if etype[:2] == evnt[0][:2]:
+                                          ef_t = headers["params"]["field_plate"]["during"][etype]
+                            if ef_t and (ef[0] == [] or ef[0][-1] == '0'):
+                                   ef[0].append('d1'), ef[1].extend([running_total, running_total + tiny_step]) 
+                                   ef[2].extend([rf"$ramp up$", rf"$ef_on$"])
+                                   tags.append(f'[3^:{running_total}][{3}:{running_total + tiny_step}] Ramp')
+                                   tags.append(f'[3:{running_total}]+[{3}:{running_total + tiny_step}] \
+                                          {round(headers["params"]["field_plate"]["ramp_time"].to("ms").magnitude, 2)}ms') 
+                                   for i in tdig:
+                                          # print(tdig[i][0])
+                                          if tdig[i][0][-1] == '1':
+                                                 # print("in")
+                                                 tdig[i][0].append('0')
+                                                 tdig[i][1].append(running_total)
+                                   running_total += tiny_step
+                                   tags.append(f'[{3}v:{running_total}][{3}:{running_total + evtime}] EF: {round(headers["params"]["field_plate"]["amplitude"]*2.5/2**15,2)}V')
+                                   tags.append(f'[{3}:{running_total}]-[{3}:{running_total + evtime}] {round(evnt[1]*cycle_len, 2)}ms')
+                            elif (not ef_t) and (ef[0] == [] or ef[0][-1] == 'd1'):
+                                   ef[0].append('0'), ef[1].append(running_total)
 
-                     # Add each event to respective channel
-                     for fig in tdig:
-                            if fig == channel:
-                                   tdig[fig][0].append('1')
-                            else:
-                                   tdig[fig][0].append('0')
-                            if tdig[fig][0][-2:] in (['0', '1'], ['1', '0'], ['1', '1'], '0', '1'):
-                                   tdig[fig][1].append(running_total)
+              # Add each event to respective channel
+              for fig in tdig:
+                     if fig == channel:
+                            tdig[fig][0].append('1')
+                     else:
+                            tdig[fig][0].append('0')
+                     if tdig[fig][0][-2:] in (['0', '1'], ['1', '0'], ['1', '1'], '0', '1'):
+                            tdig[fig][1].append(running_total)
 
-                     # Add graph labels
-                     name = clean_txt(evnt[0])
-                     try:
-                            tdig[channel][2].append(name)
-                            ind = plots.index(channel)
-                     except KeyError:
-                            ind = 0
+              # Add graph labels
+              name = clean_txt(evnt[0])
+              try:
+                     tdig[channel][2].append(name)
+                     ind = plots.index(channel)
+              except KeyError:
+                     ind = 0
 
-                     tags.append(f'[{ind}^:{running_total}][{ind}^:{running_total + evtime}] {name}')
-                     tags.append(f'[{ind}:{running_total}]+[{ind}:{running_total + evtime}] {round(evnt[1]*cycle_len, 2)}ms')
-                     running_total += evtime
-
+              tags.append(f'[{ind}^:{running_total}][{ind}^:{running_total + evtime}] {name}')
+              tags.append(f'[{ind}:{running_total}]+[{ind}:{running_total + evtime}] {round(evnt[1]*cycle_len, 2)}ms')
+              running_total += evtime
+       tdig['ef'] = ef
+       
        # Final filtering
        dead_chnl = []
        for fig in tdig:
@@ -103,45 +129,15 @@ def plot(raw_data, vis='schem', scale="int"):
                      dead_chnl.append(fig)
        for chnl in dead_chnl:
               tdig.pop(chnl)
-
+       
        # Plot graph
-       schemdraw.config(fontsize = 1)
        with schemdraw.Drawing():
               logic.TimingDiagram(
                      {'signal': [{'name': f'{i}', 'wave': tdig[i][0], 'async': tdig[i][1],}
-                                  for i in tdig], 
+                                   for i in tdig], 
                      'edge': tags,
                      },
-                     ygap=.5, grid=False,  risetime= 0)
-
-       # Perhaps this works, but was not developed past POC, so wouldn't use
-       if vis == 'matplot':
-              figure, axis = plt.subplots(4, 1)
-              if scale == "real":
-                     s_times = sum([i[1] for i in seq])
-              elif scale == "int":
-                     s_times = len(seq)
-              elif scale == "log":
-                     s_times = sum([1 + np.log(i[1])/5 for i in seq])
-              xaxis = np.arange(0, s_times+1, 1)
-              graphs = {i:np.zeros(1) for i in plots}
-              for evnt in seq:
-                     channel = 'op' if evnt[0][:-2] in ['optical_', 'detect'] else evnt[0][:2]
-                     if scale == "real":
-                            evtime = evnt[1]
-                     elif scale == "int":
-                            evtime = 1
-                     elif scale == "log":
-                            evtime = np.log(evnt[1])
-                     for fig in plots:
-                            if fig == channel:
-                                   graphs[fig] = np.hstack([graphs[fig], np.ones(evtime)])
-                            else:
-                                   graphs[fig] = np.hstack([graphs[fig], np.zeros(evtime)])
-              for i in range(3):
-                     axis[i].plot(xaxis, graphs[plots[i]])
-                     axis[i].set_title(names[i])
-              plt.show()
+                     ygap=.5, grid= False, risetime= 0)
 
 
 def t_scale(params):
@@ -149,20 +145,18 @@ def t_scale(params):
        detect = params["detect"]["on_time"] + params["detect"]["off_time"]
        rf = 2*(params["rf"]["T_0"] + params["rf"]["T_ch"])
        lf = params["lf"]["durations"][0] + params["lf"]["wait_times"][0]
-       # ef = 
-       print(type(params["lf"]["durations"]))
        x = (t.to("ms").magnitude for t in [optical, detect, rf, lf])
        return (optical.to("ms").magnitude, detect.to("ms").magnitude, rf.to("ms").magnitude, lf.to("ms").magnitude)
 
+
 if __name__ == "__main__":
        data, headers = get_experiment_data(1625324)
-       params = headers["params"]
-       inp = params["sequence"]["sequence"]
-       # print(params)
-       # print(headers["params"]["ao"].keys())#["duration"])
+
+       print(headers["params"]["field_plate"].keys())#["duration"])
+       # print(headers["params"]["field_plate"]["during"])
        # print(t_scale(headers["params"]))  # .keys())
        # inp = [('optical_ac', 25), ('rf_abar_bbar', 1), ('lf_8', 1), ('rf_abar_bbar', 1), ('detect_3', 256),
        #        # ('break', 10),
        #        ('optical_cb', 25), ('optical_ac', 25), ('rf_a_b', 1), ('lf_8', 1), ('rf_a_b', 1), ('detect_6', 256),
        #        ('optical_cb', 25)]
-       plot(1625324, scale="int")
+       plot(2025322, scale="int")
